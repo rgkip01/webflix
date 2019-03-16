64f8e03a7e33347fcfadeb853952392ff9a116df
Rafael Abreu <web.nogs@gmail.com>
2019-03-16 03:41:20 -0300

    include validates for all models

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,11 @@
 class Player < ApplicationRecord
   belongs_to :movie
   belongs_to :user
+  before_create :set_star_date
+
+  private
+
+    def set_star_date
+      self.star_date ||= Time.zone.now
+    end
 end
